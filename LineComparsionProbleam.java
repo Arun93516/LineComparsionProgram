9a47de774f60cf35c40416ce8249a1f9e6f12d3f
Arun93516 <arunkumar93516@gmail.com>
2022-09-10 12:14:26 +0530

    Added: Find the LengthofLine

--- LineComparsionProbleam.java
+++ LineComparsionProbleam.java
@@ -1,11 +1,20 @@
 package timePass;
 
 public class LineComparsionProbleam {
+	static int x1 = 10;
+	static int x2 = 12;
+	static int y1 = 10;
+	static int y2 = 12;
+	static int x = (x2 - x1);
+	static int y = (y2 - y1);
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Line Comparsion Program ");
-
+		System.out.println("Welcome to Line Comparsion Program To master Branch  ");
+		int LengthOFLine = ((x * 2) + (y * 2));
+		System.out.println("The length of  Line  :" + LengthOFLine);
+		
+		
 	}
 
 }
